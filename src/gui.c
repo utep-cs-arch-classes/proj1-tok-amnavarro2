d2a3cf6c04bfa11e4bd1d931576b6c1e4af37627
Alexis <amnavarro2@miners.utep.edu>
2021-02-06 23:02:48 -0700

    Created the interface for this project and named it gui.c

---
+++ src/gui.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "tokenizer.h"
+
+int main(){
+  char c;
+  // c = getchar();
+  printf("Enter your input: ");
+  scanf("%d",&c);
+}
+
+int space_char(char c){
+
+
+
+}//end space_char
+
+int non_space_char(char c){
+
+}
