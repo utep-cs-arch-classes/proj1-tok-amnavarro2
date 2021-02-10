3588acea940e4b0684948a2a51b1cc29a6c68517
Alexis <amnavarro2@miners.utep.edu>
2021-02-10 14:13:22 -0700

    Removed the scanner and implemented a different method to request for users input and display it

--- src/gui.c
+++ src/gui.c
@@ -1,18 +1,32 @@
 #include <stdio.h>
 #include "tokenizer.h"
+#define LIMIT 100
 
 int main(){
   char c;
-  // c = getchar();
-  printf("Enter your input: ");
-  scanf("%d",&c);
+  char s[LIMIT];
+  printf("ENTER YOUR INPUT: \n");
+  
+  int i = 0;
+  while((c = getchar()) !='\n'){
+    s[i] = c;
+    i++;
+    //putchar(c);
+    }
 
-  space_char(c);
+  printf("YOUR INPUT IS: %s\n",s);
+
+  
+  //Original Idea with scanner
+  // printf("Enter your input: ");
+  // scanf("%d",&c);
+
+  //space_char(c);
 }
-//
+
 int space_char(char c){
   if(c == ' '){
-    return true;
+    return 1;
   }
 
 
