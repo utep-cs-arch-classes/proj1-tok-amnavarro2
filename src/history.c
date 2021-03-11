05270cef1cb86b43851a982a50aa1aa0ed5203a5
Alexis <amnavarro2@miners.utep.edu>
2021-03-10 20:59:50 -0700

    made the add_history method and the print_history method

--- src/history.c
+++ src/history.c
@@ -3,20 +3,23 @@
 #include "history.h"
 
 
-int main(){
-  List *list = init_history();
-
-
+//int main(){
+  // List *list = init_history();
+  //add_history(list, "hello");
+  //add_history(list, "world");
+  //add_history(list, "are you good");
   
-}//end main
+  // print_history(list);
+  
+//}//end main
 
 
 List *init_history(){
   List *list = NULL;
-  list = malloc(sizeof(List));
+  list = malloc(sizeof(List)*1);
 
   Item *item = NULL;
-  item = malloc(sizeof(Item));
+  item = malloc(sizeof(Item)*1);
 
   list -> root = item;
   item -> id = 0;
@@ -25,3 +28,42 @@ List *init_history(){
   return list;
 
 }//end init_history
+
+void add_history(List *list, char *str){
+  Item *temp = list->root; //making a copy of of current item in the root so we don't lose it
+
+  int count = 1;
+
+  
+  if(temp->id == 0){ //if we have nothing in our list then we add the id value and the str to the first position.
+  temp->id = count;
+  temp-> str = str;
+  return;
+  }//end if
+  
+  while(temp->next != NULL){
+    temp = temp->next;
+    count++;
+  }//end while
+
+  temp ->next = malloc(sizeof(Item));//allocate memory for the next position in the list.
+  temp->next->id = count+1;//indicate the next value of the id.
+  temp->next->str = str;
+  temp->next->next = NULL;
+}//end add_history
+
+
+
+
+
+
+void print_history(List *list){
+  Item *temp = list->root;//keep our head safe
+  
+  while(temp != NULL){
+    printf("the id is: %d and the word is: %s \n", temp->id, temp->str);
+    temp = temp->next;// point to the next item in our list
+    
+  }//end while
+  
+}//end print_history
