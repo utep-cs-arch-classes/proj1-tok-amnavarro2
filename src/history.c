6720a85b0301ca97d3fee5abd26472ff1c8465f1
Alexis <amnavarro2@miners.utep.edu>
2021-03-12 02:12:05 -0700

    clarified by adding extra comments to add_history

--- src/history.c
+++ src/history.c
@@ -2,14 +2,12 @@
 #include <stdlib.h>
 #include "history.h"
 
-
 //int main(){
 //List *list = init_history();
-// add_history(list, "hello");
-// add_history(list, "world");
+//add_history(list, "hello");
+//add_history(list, "world");
 //add_history(list, "are you good");
-  
-//print_history(list);
+ //print_history(list);
 
 //char *hist = get_history(list, 1);
 //printf("the word is: %s\n",hist);
@@ -44,21 +42,22 @@ void add_history(List *list, char *str){
     
     temp_list->id = node_pos;
     temp_list->str = str;
-    return; //need to return the root cause if not we will repeat the root twice in our output.
+    return; //need to return the root cause if not we will repeat the root twice in our output. Also we exit since we successfully added the first item.
 
   }//end if
   
-  while(temp_list->next != NULL){//just to count and traverse the nodes that we have in the list.
+
+while(temp_list->next != NULL){//just to count and traverse the nodes that we have in the list.
     temp_list = temp_list->next;
     node_pos++;
   }//end while
 
-  node_pos = node_pos + 1;
+ node_pos = node_pos + 1;// identify the next position in our list
   
   temp_list->next = malloc(sizeof(Item));//allocate memory for the next node in the list.
 
-  temp_list->next->str=str; //indicate the next str in the next node.
-  temp_list->next->id = node_pos;//indicate the next value of id.
+  temp_list->next->str=str; //add the next str in the next node.
+  temp_list->next->id = node_pos;//add the next value of id.
   temp_list->next->next = NULL;//make the next-next node null, but in this case it would be the last position.
   
 }//end add_history
@@ -82,7 +81,7 @@ char *get_history(List *list, int id){
     if(temp_list->id == id){//if the id in our current position is the same as the id we want then we return the str in that position.
       return temp_list->str;
     }
-    temp_list = temp->next;
+    temp_list = temp_list->next;
   }
 
   return NULL; //return NULL if the id doesn't exist.
